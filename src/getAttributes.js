c299e5a5c996e5088e8e738f6d36993c6085c54a
James Duncan <james.duncan@cru.org>
2020-03-06 16:00:43 +0700

    add boolean option to ignore or accept passed attributes

--- src/getAttributes.js
+++ src/getAttributes.js
@@ -2,18 +2,26 @@
  * Returns the Attribute selectors of the element
  * @param  { DOM Element } element
  * @param  { Array } array of attributes to ignore
+ * @param  { Array } array of attributes to accept
  * @return { Array }
  */
-export function getAttributes( el, attributesToIgnore = ['id', 'class', 'length'] )
+export function getAttributes( el, attributes = ['id', 'class', 'length'], ignore = true )
 {
   const { attributes } = el;
   const attrs = [ ...attributes ];
 
   return attrs.reduce( ( sum, next ) =>
   {
-    if ( ! ( attributesToIgnore.indexOf( next.nodeName ) > -1 ) )
-    {
-      sum.push( `[${next.nodeName}="${next.value}"]` );
+    if (ignore) {
+        if ( ! ( attributes.indexOf( next.nodeName ) > -1 ) )
+        {
+          sum.push( `[${next.nodeName}="${next.value}"]` );
+        }        
+    } else {
+        if ( ( attributes.indexOf( next.nodeName ) > -1 ) )
+        {
+          sum.push( `[${next.nodeName}="${next.value}"]` );
+        }
     }
     return sum;
   }, [] );
